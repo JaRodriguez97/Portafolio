edea8de9b64ba2eaf252ffcdfccdb78cb976e6d4
JaRodriguez97 <cristi542012@gmail.com>
2022-11-17 11:47:51 -0500

    about and glass

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-function menuToggle() {
-  const toggleMenu = document.querySelector(".banner__header--toggle"),
-    banner = document.querySelector(".banner");
+let anteriorRoute,
+  toggleMenu = document.querySelector(".banner__header--toggle"),
+  banner = document.querySelector(".banner");
 
+function menuToggle() {
   banner.classList.toggle("active");
   toggleMenu.classList.toggle("active");
 }
 
-
 function getTo(route) {
   menuToggle();
-  
+
+  if (anteriorRoute) banner.classList.remove(anteriorRoute);
+  if (route) banner.classList.toggle(route);
+
+  anteriorRoute = route;
 }
